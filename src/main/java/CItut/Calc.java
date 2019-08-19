fba19e5b0a41ee8b776ff096e2a23f7e19e3a5f0
Mark Walker <mark_w19@hotmail.com>
2019-08-19 23:50:00 +1200

    adding files

---
+++ src/main/java/CItut/Calc.java
@@ -0,0 +1,18 @@
+package CItut;
+
+
+public class Calc{
+    static int add (int x, int y){
+        return x + y;        
+    }
+
+    static int subtract (int x, int y){
+        return x + y;
+    }
+
+    public static void main( String[] args )
+    {
+       System.out.println(add(3,4));
+       System.out.println(add(5,6)); 
+    }
+}
---
+++ src/test/java/CItut/TestCalc.java
@@ -0,0 +1,19 @@
+package CItut;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import CItut.Calc;
+
+public class TestCalc {
+//    @Test
+//     public void testCalc() {
+//         assertEquals(7, new Calc().subtract(10, 3));
+//     }
+   
+   @Test
+	public void testCalc() {
+		assertEquals(5, new Calc().add(2, 3));
+	}
+
+    
+}
